0a235d60292f0269d61eddb010316f27fdc413fe
pksoo <kwangge95@gmail.com>
2026-08-05 00:00:31 +0900

    Using provided enum instead of redundant custom #defines to reduce compile overhead.

--- nrf52_drv_gpio.h
+++ nrf52_drv_gpio.h
@@ -2,13 +2,11 @@
 #define NRF52_DRV_GPIO_H__
 
 
-#include <stdio.h>
 #include <stdint.h>
 
 #include "nrfx_gpiote.h"
-#include "boards.h"
 
-#include "user_define.h"
+
 #include "input_ctrl.h"
 
 
@@ -16,25 +14,31 @@
 extern "C"{
 #endif
 
-#define OUTPUT              0x00
-#define INPUT               0x01
 
 #define HIGH_STATE          0x01
 #define LOW_STATE           0x00
 
 
+
+typedef enum
+{
+    INPUT,
+    OUTPUT
+}eDirect_t;
+
+
 typedef struct 
 {
-    nrf_gpiote_polarity_t   edge;
     nrf_gpio_pin_pull_t     pull_config;
     nrf_gpiote_outinit_t    init;
-    uint8_t pin_number;
-    uint8_t direct;
-}_strGPIO;
+    nrf_gpiote_polarity_t   edge;
+    eDirect_t               direct;
+    uint8_t                 pin_number;
+}sGPIO_config_t;
 
 
 
-void Init_GPIO(_strGPIO config);
+void Init_GPIO(sGPIO_config_t config);
 uint32_t Read_GPIO(uint32_t pin);
 void Write_GPIO_State(uint32_t pin, uint8_t state);
 
